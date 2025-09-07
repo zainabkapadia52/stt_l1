1c8392ada3baa925339706f0e9fc424a12159ff9
zainabkapadia52 <23110373@iitgn.ac.in>
2025-09-07 17:51:00 +0530

    indent res

--- error.py
+++ error.py
@@ -1,5 +1,4 @@
-x="Racecar"
-
+x="civic"
 def ispalindrome(word):   
      # function to check palindrome
   rev=word[::-1]   
@@ -7,24 +6,18 @@ def ispalindrome(word):
    return True
   else:
         return False
-    
 def result(text):
   if ispalindrome(text):
         print("good")
   else:
    print("bad")
-
-
 def check_multiple():
         words=["mom","hello","cry","moon","level"]
         for w in words:print(w,ispalindrome(w))
-
-
 # extra unused variables
 a=100
 b=200
 c=300
-
 # misplaced indentation and bad naming
 def BadFunc():
      for i in range(5):
@@ -34,10 +27,9 @@ def BadFunc():
                    print("not middle")
      return None
      print("never runs")
-
 #wrong code
 result(x)
 check_multiple()
 BadFunc()
 print("Done")
-   print("extra indent") # error
+print("extra indent") # error
